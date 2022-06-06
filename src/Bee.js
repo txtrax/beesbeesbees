af8a1ebc6a36ef71950df92102b3e5653c513c21
Theresa Tran <theresa.tran216@gmail.com>
2022-06-06 11:55:50 -0700

    Completed assignment

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  // call the Grub superclass
+  Grub.call(this);
+  //don't hard code inside call
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// set the prototype
+// set the constructor
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
 var Grub = function() {
+  // Create a Grub class, in pseudoclassical style
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {
+
+};
